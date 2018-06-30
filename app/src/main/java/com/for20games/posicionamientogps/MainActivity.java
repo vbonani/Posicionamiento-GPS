726a7d939775cb01c64874f1894fcac78874f484
vBonani <vbonani@fi.uba.ar>
2018-06-30 17:54:02 -0300

    Con Dependencias GPS Agregadas

--- app/src/main/java/com/for20games/posicionamientogps/MainActivity.java
+++ app/src/main/java/com/for20games/posicionamientogps/MainActivity.java
@@ -3,11 +3,30 @@ package com.for20games.posicionamientogps;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
+    private LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback() {
+            @Override public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                //usamos locationResult.getLastLocation() para tener
+                //la ubicación más reciente
+            }
+        };
+
+
     }
 }
